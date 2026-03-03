9acf8676e72034dbef093809f93e8271788df096
Ritik Shreekant Pandey <ritikpandey148@gmail.com>
2026-03-03 11:37:08 +0530

    message

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
-  if (err) {
-    console.log("❌ DB Connection Failed");
-    console.log(err);
-  } else {
-    console.log("✅ MySQL Connected Successfully");
-  }
+    if (err) {
+        console.log("❌ DB Connection Failed");
+        console.log(err);
+    } else {
+        console.log("✅ MySQL Connected Successfully");
+    }
 });
 
 module.exports = db;
