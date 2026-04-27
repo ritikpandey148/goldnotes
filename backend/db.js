5894d466411fd7cf7ae2aa9d1542c95e5857cc91
Ritik Pandey <ritikpandey148@gmail.com>
2026-04-27 20:48:50 +0530

    final mysql fix

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,31 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-db.connect((err) => {
-  if (err) {
+// Test connection
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    console.log("✅ MySQL Connected Successfully");
+    connection.release();
+  } catch (err) {
     console.log("❌ DB Connection Failed");
     console.log(err);
-  } else {
-    console.log("✅ MySQL Connected Successfully");
   }
-});
+})();
 
 module.exports = db;
